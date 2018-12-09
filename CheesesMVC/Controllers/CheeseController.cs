c124a0b526557260e5619c58ebe66c29966e3bd5
Jennifer Flores <jlflores@charter.net>
2018-12-08 19:49:45 -0600

    model update

--- CheesesMVC/Controllers/CheeseController.cs
+++ CheesesMVC/Controllers/CheeseController.cs
@@ -3,21 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using CheesesMVC.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace CheesesMVC.Controllers
+
 {
     public class CheeseController : Controller
     {
 
-        static private List<string> Cheeses = new List<string>();
+        
+
 
         // GET: /<controller>/
         public IActionResult Index()
         {
            
-            ViewBag.cheeses = Cheeses;
+            ViewBag.cheeses = CheeseData.GetAll();
 
             return View();
         }
@@ -26,14 +29,36 @@ namespace CheesesMVC.Controllers
             return View();
         }
         [HttpPost]
-        [Route("/Cheese/add")]
-        public IActionResult NewCheese(string name)
+        [Route("/Cheese/Add")]
+        public IActionResult NewCheese(Cheese newCheese)
         {
+
             //add new cheese to previous cheeses
-            Cheeses.Add(name);
+            CheeseData.Add(new Cheese);
+
+            
 
             return Redirect("/Cheese");
 
+        }
+
+        public IActionResult Remove()
+        {
+            ViewBag.title = "Remove Cheeses";
+            ViewBag.cheese = CheeseData.GetAll;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Remove(int[] cheeseIds)
+        {
+            //todo-  remove cheeses from list
+            foreach (int cheeseId in cheeseIds)
+            {
+                CheeseData.Remove(cheeseId);
+            }
+            
+            return Redirect("/");
         }
         
     }
---
+++ CheesesMVC/Models/CheeseData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheesesMVC.Models
+{
+    public class CheeseData
+    {
+        static private List<Cheese> cheeses = new List<Cheese>();
+        //get all of data to the methods
+        public static List<Cheese> GetAll()
+        {
+            return cheeses;
+        }
+
+
+        //add method
+        public static void Add(Cheese newCheese)
+        {
+            cheeses.Add(newCheese);
+        }
+
+        //remove method
+        public static void Remove(int id)
+        {
+            Cheese cheesetoRemove = GetById(id);
+            cheeses.Remove(cheesetoRemove);
+        }
+
+        //getbyid
+        public static Cheese GetById(int id)
+        {
+            return cheeses.Single(x => x.cheeseId == id);
+        }
+    }
+}
